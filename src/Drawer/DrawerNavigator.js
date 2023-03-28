50a6935221b83dfe89680b2e2ec64e3229f7d298
Sandeep Painkra <samdeeppainkra@gmail.com>
2023-03-29 01:17:24 +0530

    this is my second commit

--- src/Drawer/DrawerNavigator.js
+++ src/Drawer/DrawerNavigator.js
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Main from './Main';
+import About from './About';
 
 const Drawer = createDrawerNavigator();
 
@@ -9,6 +10,7 @@ const DrawerNavigator = () => {
   return (
     <Drawer.Navigator>
       <Drawer.Screen name="Main" component={Main} options={{headerShown:false}} />
+      <Drawer.Screen name='About' component={About}/>
     </Drawer.Navigator>
   )
 }
